64200618890512c61644426b38481fb0e2eefbcf
tsalgie <16627886+tsalgie@users.noreply.github.com>
2020-01-10 23:15:38 -0600

    naively factor out iracing client methods

--- application/api/api_routes.py
+++ application/api/api_routes.py
@@ -7,31 +7,31 @@ api = Blueprint('api_bp', __name__,
                 template_folder='templates',
                 url_prefix='/api/v1')
 
+
+def decrease_multiplier(multiplier, multiplier_floor):
+    if timed_race():
+        half_race_time = total_race_time() / 2  # in seconds
+        decrease_mult_amount_each_second = (multiplier - multiplier_floor) / half_race_time
+        multiplier -= elapsed_seconds() * decrease_mult_amount_each_second
+    else:
+        half_race_laps = total_race_laps() / 2
+        decrease_mult_amount_each_lap = (multiplier - multiplier_floor) / half_race_laps
+        multiplier -= elapsed_laps() * decrease_mult_amount_each_lap
+
+
 @api.route('/multipliers/win', methods=['GET'])
 def win_multiplier():
-    irsdk()
     multiplier_floor = 1.15
-    standings = self.iracing_client.standings()
-    current_position = standings.index(app.config['IRACING_USER'])  # 0 indexed
-
-    session_status = self.iracing_client.session_status()
-    green_light = session_status['green']  # boolean
+    current_position = car_position()
 
     multiplier = 10 + current_position
 
-    # after green light, divide by 4
-    if green_light:
+    # after lights out, divide by 4
+    if session_status() == LIGHTS_OUT:
         multiplier /= 4.0
 
-    race_type = 'laps'  # or 'timed'
-    if race_type == 'laps':
-        half_race_laps = session_status['total_race_laps'] / 2
-        decrease_mult_amount_each_lap = multiplier - multiplier_floor / half_race_laps
-        multiplier -= (session_status['elapsed_laps'] * decrease_mult_amount_each_lap)
-    else:
-        half_race_time = session_status['total_race_time'] / 2  # in seconds
-        decrease_mult_amount_each_second = multiplier - multiplier_floor / half_race_time
-        multiplier -= (session_status['elapsed_seconds'] * decrease_mult_amount_each_second)
+    # value decreases throughout the race
+    decrease_multiplier(multiplier, multiplier_floor)
 
     return multiplier
 
@@ -39,16 +39,16 @@ def win_multiplier():
 @api.route('/multipliers/top5', methods=['GET'])
 def top5_multiplier():
     multiplier_floor = 1.15
-    standings = self.iracing_client.standings()
-    current_position = standings.index(app.config['IRACING_USER'])  # 0 indexed
-
-    session_status = self.iracing_client.session_status()
-    green_light = session_status.json['green']  # boolean
+    current_position = car_position()
 
     multiplier = 5 + ((current_position - 4) / 2)
-    # value actually decreases throughout the race
+
     # after green light, divide by 3
-    # how often does total get decreased? by how much?
+    if session_status() == LIGHTS_OUT:
+        multiplier /= 3.0
+
+    # value decreases throughout the race
+    decrease_multiplier(multiplier, multiplier_floor)
 
     return multiplier
 
@@ -56,17 +56,17 @@ def top5_multiplier():
 @api.route('/multipliers/finish', methods=['GET'])
 def finish_multiplier():
     multiplier_floor = 1.15
-    standings = self.iracing_client.standings()
-    current_position = standings.index(app.config['IRACING_USER'])  # 0 indexed
+    current_position = car_position()
+    grid_size = field_size()
 
-    session_status = self.iracing_client.session_status()
-    green_light = session_status.json['green']  # boolean
+    multiplier = 1.5 + (abs(current_position-grid_size/2) / grid_size)  # between 1.5 and 2 depending on distance from med pos
 
-    field_size = 0  # http
-    multiplier = 1.5  # between 1.5 and 2 depending on distance from med pos
-    # value actually decreases throughout the race
     # after green light, divide by 2
-    # how often does total get decreased? by how much?
+    if session_status() == LIGHTS_OUT:
+        multiplier /= 2.0
+
+    # value decreases throughout the race
+    decrease_multiplier(multiplier, multiplier_floor)
 
     return multiplier
 
@@ -74,17 +74,16 @@ def finish_multiplier():
 @api.route('/multipliers/crash', methods=['GET'])
 def crash_multiplier():
     multiplier_floor = 1.15
-    standings = self.iracing_client.standings()
-    current_position = standings.index(app.config['IRACING_USER'])  # 0 indexed
+    current_position = car_position()
+    grid_size = field_size()
 
-    session_status = self.iracing_client.session_status()
-    green_light = session_status.json['green']  # boolean
+    multiplier = 2 + (4 * abs(current_position-grid_size/2) / grid_size)  # between 2 and 4 depending on distance from med pos
 
-    field_size = 0  # http
-    multiplier = 2  # between 2 and 4 depending on distance from med pos
-    # value actually decreases throughout the race
     # after green light, divide by 2
-    # how often does total get decreased? by how much?
+    if session_status() == LIGHTS_OUT:
+        multiplier /= 2.0
+
+    # value decreases throughout the race
+    decrease_multiplier(multiplier, multiplier_floor)
 
     return multiplier
-'''
--- application/api/iracing_client.py
+++ application/api/iracing_client.py
@@ -1,6 +1,53 @@
 #!python3
 from flask import current_app as app
 
+GRID = 0
+LIGHTS_OUT = 1
+FINISHED = 2
+SESSION_DONE = 3
+CRASHED = 3
+
 
 def irsdk():
     app.config['IR']
+
+
+def field_size():
+    pass
+
+
+def car_position():
+    #standings().index(app.config['IRACING_USER'])  # 0 indexed
+    pass
+
+
+def car_status():
+    pass
+
+
+def elapsed_laps():
+    pass
+
+
+def elapsed_seconds():
+    pass
+
+
+def session():
+    pass
+
+
+def session_status():
+    pass
+
+
+def timed_race():
+    pass
+
+
+def total_race_laps():
+    pass
+
+
+def total_race_time():
+    pass
